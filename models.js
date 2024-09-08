330d9eb45ce32b066534efa0d01a071482768849
Martin Shanahan <m.shanahan914@gmail.com>
2024-09-08 22:32:21 +0100

    added search functionality

--- models.js
+++ models.js
@@ -1,16 +1,21 @@
 import pg from "pg";
 const { Pool } = pg;
+const pw = process.env.PGPASSWORD
 
 export const pool = new Pool({
   user: "my_role",
   host: "localhost",
   database: "blog_api",
-  password: "my_password",
+  password: pw,
   port: 5432,
 });
 
+//gets all posts
+//optional search parameter 'term' can be added. Searches in title, content and category. 
 export const getAllPosts = (req, res, next) => {
-  pool.query("SELECT * FROM posts", (error, result) => {
+    const term = req.query.term ? req.query.term : "";
+    const searchTerm = `%${term}%`
+  pool.query("SELECT * FROM posts WHERE title ILIKE $1 OR content ILIKE $1 OR category ILIKE $1", [searchTerm], (error, result) => {
     if (error) {
       res.status(500).json({ msg: "internal server error" });
       return;
@@ -19,6 +24,7 @@ export const getAllPosts = (req, res, next) => {
   });
 };
 
+//gets a single post
 export const getSinglePost = (req, res, next) => {
   const id = parseInt(req.params.id);
   pool.query("SELECT * FROM posts WHERE id = $1", [id], (error, result) => {
@@ -35,10 +41,13 @@ export const getSinglePost = (req, res, next) => {
   });
 };
 
+//updates an existing post
 export const updatePost = (req, res, next) => {
   const id = parseInt(req.params.id);
   const { title, content, category, tags } = req.body;
-  console.log(tags)
+  if (!title || !content || !category || !tags){
+    return res.status(400).json({msg: "you must provide the following fields: title, content, category and tags"})
+}
   pool.query(
     "UPDATE posts SET title=$1, content=$2, category=$3, tags=$4, updatedAt=DEFAULT  WHERE id = $5 RETURNING *",
     [title, content, category, tags, id],
@@ -49,7 +58,7 @@ export const updatePost = (req, res, next) => {
       console.log(result.rows.length);
       if (result.rows.length === 0) {
         return res
-          .status(400)
+          .status(404)
           .json({ msg: `no post with an id of ${id} was found` });
       }
       res.status(200).json(result.rows[0]);
@@ -57,26 +66,36 @@ export const updatePost = (req, res, next) => {
   );
 };
 
+//creates a new post
 export const createPost = (req, res, next) => {
     const { title, content, category, tags } = req.body;
+    if (!title || !content || !category || !tags){
+        return res.status(400).json({msg: "you must provide the following fields: title, content, category and tags"})
+    }
     pool.query(
         'INSERT INTO posts (title, content, category, tags) VALUES ($1, $2, $3, $4) RETURNING *', [title, content, category, tags], 
         (error, result) => {
             if (error){
                 return res.status(500).json({ msg: "Internal server error" });
             }
-            res.status(200).json(result.rows)
+            res.status(201).json(result.rows)
         }
     )
 }
 
+//deletes an existing post
 export const deletePost = (req, res, next) => {
     const id = parseInt(req.params.id)
-    pool.query('DELETE FROM posts WHERE id=$1', [id], (error, result) =>{
+    pool.query('DELETE FROM posts WHERE id=$1 RETURNING *', [id], (error, result) =>{
         if (error){
             console.error('Error', error)
             return res.status(500).json({ msg: "Internal server error" });
         }
+        if (result.rows.length === 0) {
+            return res
+              .status(404)
+              .json({ msg: `no post with an id of ${id} was found` });
+          }
         res.status(204).send('user deleted')
     })
 }
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import routes from './routes.js';
-import errorHandler from './middleware/error.js';
 
 const port = process.env.PORT || 8000
 
@@ -9,13 +8,5 @@ const app = express();
 app.use(express.json())
 app.use('/api', routes)
 
-app.use((req,res,next) =>{
-    const error = new Error('not found')
-    error.status = 404
-    next(error)
-})
-
-//errorHander - middelware for errors
-app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
